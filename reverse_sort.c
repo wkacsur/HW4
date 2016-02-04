7d27c2052f59a4209096467012223d08c68452e8
wkacsur <wkacsur@umich.edu>
2016-02-04 15:06:22 -0500

    Update reverse_sort.c

--- reverse_sort.c
+++ reverse_sort.c
@@ -1,1 +1,18 @@
 
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+int compare(int a, int b) {
+	    return b - a;
+}
+
+int compare_p(const void *a, const void *b)
+{
+	const int *aa, *bb;
+	aa = a;
+	bb = b;
+	return *bb - *aa;
+}
